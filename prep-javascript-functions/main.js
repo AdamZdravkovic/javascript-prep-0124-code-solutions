c87f07ca2140aa867acf726277be7683c1ef1bd2
Adam Zdravkovic <adambusiness24@gmail.com>
2024-01-12 16:50:05 +0000

    Prep JavaScript Functions - Adam Zdravkovic

---
+++ prep-javascript-functions/main.js
@@ -0,0 +1,53 @@
+function addTwoNumbers(num1, num2) {
+  return num1 + num2;
+}
+
+console.log('addTwoNumbers Exercise:', addTwoNumbers(2, 2));
+
+function convertHoursToMinutes(hours) {
+  return hours * 60;
+}
+
+console.log('convertHoursToMinutes Exercise:', convertHoursToMinutes(2));
+
+function getGreeting(name) {
+  return 'Hello ' + name + '!';
+}
+
+console.log('getGreeting Exercise:', getGreeting('World'));
+
+function addAndMultiplyBy5(num1, num2) {
+  return (num1 + num2) * 5;
+}
+
+console.log('addAndMultiplyBy5 Exercise:', addAndMultiplyBy5(10, 5));
+
+function multiplyAndDivideBy5(num1, num2) {
+  return (num1 * num2) / 5;
+}
+
+console.log('multiplyAndDivideBy5 Exercise:', multiplyAndDivideBy5(35, 10));
+
+function subtractTwoNumbers(num1, num2) {
+  return num1 - num2;
+}
+
+console.log('subtractTwoNumbers Exercise:', subtractTwoNumbers(22, 7));
+
+function getCircleCircumference(radius) {
+  return 2 * Math.PI * radius;
+}
+
+console.log('getCircleCircumference Exercise:', getCircleCircumference(5));
+
+function fullName(firstName, lastName) {
+  return firstName + ' ' + lastName;
+}
+
+console.log('fullName Exercise:', fullName('Juan', 'Ramirez'));
+
+function cube(number) {
+  return number * number * number;
+}
+
+console.log('cube Exercise:', cube(5));
